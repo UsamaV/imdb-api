edc636efe7a1b83e416bae63d11453baa0028a3d
UsamaV <usamanasir23@gmail.com>
2025-05-22 16:31:26 -0400

    fix: code cleanup

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -2,6 +2,6 @@ class MovieSerializer < ActiveModel::Serializer
   attributes :id, :title, :year, :rating, :director, :plot, :poster_url
 
   has_many :casts, through: :movie_casts, serializer: CastSerializer do
-    object.casts.limit(3) # Only show top 3 cast members
+    object.casts.limit(3)
   end
 end
--- test/models/cast_test.rb
+++ test/models/cast_test.rb
@@ -1,7 +1,4 @@
 require "test_helper"
 
 class CastTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 end
